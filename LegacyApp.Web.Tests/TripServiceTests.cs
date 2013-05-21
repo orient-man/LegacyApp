27a2b59614a06c8cd60e075f0c4a0019d43b4749
malin <malin@piatka.pl>
2013-05-21 17:51:10 +0200

    Test: should not return any trips when users are not friends...

--- LegacyApp.Web.Tests/TripServiceTests.cs
+++ LegacyApp.Web.Tests/TripServiceTests.cs
@@ -9,17 +9,38 @@ namespace LegacyApp.Web.Tests
     {
         private readonly User Guest = null;
         private readonly User UnusedUser = null;
+        private readonly User RegisteredUser = new User { Name = "Alice" };
+        private readonly User AnotherUser = new User { Name = "Bob" };
+        private readonly Trip ToBrazil = new Trip();
         private static User loggedInUser;
+        private TripService tripService;
 
         [Test, ExpectedException(typeof (UserNotLoggedInException))]
         public void ShouldThrowAnExceptionWhenNotLoggedIn()
         {
             // arrange
-            TripService tripService = new TestableTripService();
+            tripService = new TestableTripService();
             loggedInUser = Guest;
 
             // act
             tripService.GetTripsByUser(UnusedUser);
+        }
+
+        [Test]
+        public void ShouldNotReturnAnyTripsWhenUsersAreNotFriends()
+        {
+            // arrange
+            tripService = new TestableTripService();
+            loggedInUser = RegisteredUser;
+            var friend = new User();
+            friend.Friends.Add(AnotherUser);
+            friend.Trips.Add(ToBrazil);
+
+            // act
+            var friendTrips = tripService.GetTripsByUser(friend);
+
+            // assert
+            Assert.That(friendTrips, Is.Empty);
         }
 
         private class TestableTripService : TripService
