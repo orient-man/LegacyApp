9f05ac702ed22c60b283b2f0a14f1c4c457fbaec
malin <malin@piatka.pl>
2013-05-21 17:14:08 +0200

    First unit test but failing (ignored for now) because of dependecy problem

---
+++ LegacyApp.Web.Tests/TripServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using LegacyApp.Web.Services;
+
+namespace LegacyApp.Web.Tests
+{
+    [TestFixture]
+    public class TripServiceTests
+    {
+        [Ignore, Test, ExpectedException(typeof (UserNotLoggedInException))]
+        public void ShouldThrowAnExceptionWhenNotLoggedIn()
+        {
+            // arrange
+            TripService tripService = new TripService();
+
+            // act
+            tripService.GetTripsByUser(null);
+        }
+    }
+}
